cf57f12401fe2dcf5a0df3bb54e99cfd645a06e5
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-03-28 12:12:55 +0900

    # add : boj 10845 큐

---
+++ Algorithm/Queue/10845 큐.js
@@ -0,0 +1,75 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+const N = parseInt(input[0]);
+let result = "";
+
+class Queue {
+  constructor(N) {
+    this.arr = new Array(N);
+    this.forward = 0;
+    this.rear = 0;
+  }
+
+  push(num) {
+    this.arr[this.rear] = num;
+    this.rear++;
+  }
+
+  pop() {
+    if (this.forward === this.rear) return -1;
+
+    return this.arr[this.forward++];
+  }
+
+  size() {
+    return this.rear - this.forward;
+  }
+
+  empty() {
+    return this.forward === this.rear ? 1 : 0;
+  }
+
+  front() {
+    if (this.forward === this.rear) return -1;
+
+    return this.arr[this.forward];
+  }
+
+  back() {
+    if (this.forward === this.rear) return -1;
+
+    return this.arr[this.rear - 1];
+  }
+}
+
+const queue = new Queue(N);
+
+for (let i = 1; i <= N; i++) {
+  const command = input[i].split(" ");
+
+  switch (command[0]) {
+    case "push":
+      queue.push(parseInt(command[1]));
+      break;
+    case "pop":
+      result += queue.pop() + "\n";
+      break;
+    case "size":
+      result += queue.size() + "\n";
+      break;
+    case "empty":
+      result += queue.empty() + "\n";
+      break;
+    case "front":
+      result += queue.front() + "\n";
+      break;
+    case "back":
+      result += queue.back() + "\n";
+      break;
+  }
+}
+
+console.log(result);
