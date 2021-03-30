2b525e91512c227a190cb066e1e133df11529a68
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-03-30 22:27:19 +0900

    # add boj 자료구조 (1158, 1406) 문제 풀이

---
+++ Algorithm/Stack/1406 에디터.js
@@ -0,0 +1,33 @@
+var fs = require("fs");
+var input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let strings = input.shift();
+let M = input.shift();
+
+let leftStack = strings.split("");
+let rightStack = [];
+
+for (let i = 0; i < M; i++) {
+  let command = input[i].split(" ");
+  switch (command[0]) {
+    case "L":
+      if (leftStack.length != 0) {
+        rightStack.push(leftStack.pop());
+      }
+      break;
+    case "D":
+      if (rightStack.length != 0) {
+        leftStack.push(rightStack.pop());
+      }
+      break;
+    case "B":
+      if (leftStack.length != 0) {
+        leftStack.pop();
+      }
+      break;
+    case "P":
+      leftStack.push(command[1]);
+      break;
+  }
+}
+let result = leftStack.concat(rightStack.reverse()).join("");
+console.log(result);
