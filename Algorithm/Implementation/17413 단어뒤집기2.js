cf54dd55ac43aa992d681a1ff1faf16c891d0fdf
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-04-03 23:32:52 +0900

    # add : boj 자료구조 문제 풀이

--- Algorithm/Implementation/17413 단어뒤집기2.js
+++ Algorithm/Implementation/17413 단어뒤집기2.js
@@ -4,21 +4,20 @@ let input = require("fs").readFileSync("/dev/stdin", "utf8"),
   ans = "";
 
 for (let a of input) {
-  // input 의 각각 열을 확인한다.
   if (a === "<") {
-    isTag = true; // true로 바꿔주면 ">" 가 필요하다는 시그널로 사용이 가능하다
-    ans += temp.split("").reverse().join("") + a; // "<" 앞을 뒤집어 주는 이유는 <open> tag <close> 라고 한다면 open 앞에는 비어있으니까 패스하고,
-    // close 앞에 있는 temp에 담겨있는 tag를 뒤집어주면된다.
+    isTag = true;
+    ans += temp.split("").reverse.join("") + a;
     temp = "";
   } else if (a === ">") {
     isTag = false;
     ans += temp + a;
     temp = "";
   } else if (a === " ") {
-    ans += (!isTag ? temp.split("").reverse().join("") : temp) + " ";
+    ans += (isTag ? temp.split("").reverse().join("") : temp) + " ";
     temp = "";
   } else {
     temp += a;
   }
 }
+
 console.log((ans + temp.split("").reverse().join("")).replace(/\n/g, ""));
