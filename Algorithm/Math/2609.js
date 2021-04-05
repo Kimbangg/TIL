fb16a421f92499b7b6693df3a2d2395826246157
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-04-05 16:56:57 +0900

    # add : boj(2609) 최대공약수와 최소공배수 문제풀이

---
+++ Algorithm/Math/2609.js
@@ -0,0 +1,19 @@
+function solution() {
+  let a = A;
+  let b = B;
+
+  while (b > 0) {
+    const r = a % b;
+    a = b;
+    b = r;
+  }
+
+  console.log(a);
+  console.log((A * B) / a);
+}
+
+const input = require("fs").readFileSync("/dev/stdin", "utf8").trim();
+
+const [A, B] = input.split(" ").map((c) => +c);
+
+solution();
