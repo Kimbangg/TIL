4658f645cc91bb44d49985a894297b65088275d4
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-04-18 16:34:16 +0900

    #add : boj(2193) 이친수 문제 풀이

---
+++ Algorithm/DP/2193 이친수.js
@@ -0,0 +1,9 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+let dp = [0, BigInt("1"), BigInt("1")];
+
+for (let i = 3; i <= parseInt(input); i++) {
+  dp[i] = dp[i - 1] + dp[i - 2];
+}
+
+console.log(dp[parseInt(input)].toString()dp
