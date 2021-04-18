8d7d47bd06abc1a1a1b83d257dcd57507b33c1be
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-04-18 18:35:09 +0900

    #add : boj(11053) 가장긴 증가하는 부분 수열  문제 풀이

---
+++ Algorithm/DP/11053 가장 긴 증가하는 부분 수열.js
@@ -0,0 +1,15 @@
+let input = require("fs").readFileSync("dev/stdin").toString().split("\n");
+let n = Number(input[0]);
+let seq = input[1].split(" ").map((num) => parseInt(num));
+const dp = Array(n + 1).fill(0);
+seq.unshift(0);
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < i; j++) {
+    if (seq[i] > seq[j] && dp[i] < dp[j]) {
+      dp[i] = dp[j];
+    }
+  }
+  dp[i] += 1;
+}
+
+console.log(Math.max(dp));
