eb2fc622537c8a992fb8d03b832c9ea1bbc157ac
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-04-22 19:20:42 +0900

    # add : boj (11501) 주식 문제 풀이

---
+++ Algorithm/Greedy/11501 주식.js
@@ -0,0 +1,26 @@
+function solution(stock) {
+  let result = 0;
+  let maxStock = stock[stock.length - 1];
+  for (let i = stock.length - 2; i >= 0; i--) {
+    if (stock[i] < maxStock) {
+      result += maxStock - stock[i];
+    } else {
+      maxStock = stock[i];
+    }
+  }
+  console.log(result);
+}
+
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+
+const T = Number(input[0]);
+
+for (let i = 0; i < T; i++) {
+  const N = Number(input[i]);
+  let stock = [];
+  for (let j = 0; j < N; j++) {
+    stock[j] = Number(input[j]);
+  }
+  solution(stock);
+}
---
+++ Algorithm/Greedy/11501 주식.py
@@ -0,0 +1,13 @@
+T = int(input());
+
+for _ in range(T):
+    n = int(input())
+    stock = list(map(int,input().split()))
+    answer = 0 
+    maxStock = stock[-1]
+    for i in range(n-2,-1,-1):
+        if stock[i] > mx: 
+            maxStock = stock[i]
+        else:
+            answer += maxStock-stock[i]
+    print(answer)
