60598fd1eaed835e17aec8708dfbf212762fefdd
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-04-27 22:50:39 +0900

    # refactor : 프로그래머스 프린터 문제

--- Algorithm/Queue/프로그래머스 프린터.js
+++ Algorithm/Queue/프로그래머스 프린터.js
@@ -1,3 +1,5 @@
+// 리팩토링 이전
+
 function solution(priorities, location) {
   let answer = 0;
   let target_index = location;
@@ -27,4 +29,31 @@ function solution(priorities, location) {
   return answer;
 }
 
-// input : [2,1,3,2]
+// 리팩토링 이후
+
+function solution(priorities, location) {
+  let answer = 0;
+  let target_index = location;
+
+  while (priorities.length) {
+    if (priorities.some((num) => num > priorities[0])) {
+      priorities.push(priorities[0]);
+      priorities.shift(priorities[0]);
+    } else {
+      priorities.shift(priorities[0]);
+      answer += 1;
+
+      if (target_index === 0) {
+        break;
+      }
+    }
+
+    target_index -= 1;
+
+    if (target_index === -1) {
+      target_index = priorities.length - 1;
+    }
+  }
+
+  return answer;
+}
