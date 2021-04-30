182f294a82bb567525a96337b95720f45914613b
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-04-30 13:31:57 +0900

    # add : boj(1181) 단어 정렬 문제 풀이

---
+++ Algorithm/Sorting/1181 단어정렬.js
@@ -0,0 +1,20 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+input.shift();
+
+input.sort((a, b) => {
+  // 길이가 같으면, 사전 순서대로
+  if (a.length === b.length) {
+    return a > b ? 1 : -1;
+  }
+  return a.length - b.length;
+});
+
+// 중복제거
+const answer = [...new Set(input)];
+
+console.log(answer.join("\n"));
