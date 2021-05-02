225155915993d2c24bfa30a34576ad34f0fc8e57
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-02 23:06:36 +0900

    # add : boj (1406) 에디터 문제 리팩

--- Algorithm/Stack/1406 에디터.js
+++ Algorithm/Stack/1406 에디터.js
@@ -1,33 +1,36 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+function solution() {
+  for (let i = 0; i < M; i++) {
+    let command = input[i].split(" ");
+    switch (command[0]) {
+      case "L":
+        if (leftStack.length != 0) {
+          rightStack.push(leftStack.pop());
+        }
+        break;
+      case "D":
+        if (rightStack.length != 0) {
+          leftStack.push(rightStack.pop());
+        }
+        break;
+      case "B":
+        if (leftStack.length != 0) {
+          leftStack.pop();
+        }
+        break;
+      case "P":
+        leftStack.push(command[1]);
+        break;
+    }
+  }
+  let result = leftStack.concat(rightStack.reverse()).join("");
+  console.log(result);
+}
+
+var fs = require("fs");
+var input = fs.readFileSync("/dev/stdin").toString().split("\n");
 let strings = input.shift();
 let M = input.shift();
 
 let leftStack = strings.split("");
 let rightStack = [];
-
-for (let i = 0; i < M; i++) {
-  let command = input[i].split(" ");
-  switch (command[0]) {
-    case "L":
-      if (leftStack.length != 0) {
-        rightStack.push(leftStack.pop());
-      }
-      break;
-    case "D":
-      if (rightStack.length != 0) {
-        leftStack.push(rightStack.pop());
-      }
-      break;
-    case "B":
-      if (leftStack.length != 0) {
-        leftStack.pop();
-      }
-      break;
-    case "P":
-      leftStack.push(command[1]);
-      break;
-  }
-}
-let result = leftStack.concat(rightStack.reverse()).join("");
-console.log(result);
+solution();
