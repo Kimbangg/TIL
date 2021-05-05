5eed9d25fb37b65e3f9ba442db34bb3b8c2e4157
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-05 13:59:38 +0900

    # add : boj(1157) 단어 공부 문제 풀이

---
+++ Algorithm/Sorting/1157 단어 공부.js
@@ -0,0 +1,20 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().toLowerCase();
+
+let isSame = false;
+const arr = new Array(26).fill(0);
+
+for (let i = 0; i < input.length; i++) {
+  arr[input.charCodeAt(i) - 97]++;
+}
+
+const max = Math.max(...arr);
+const index = arr.indexOf(max);
+
+for (let j = 0; j < 26; j++) {
+  if (arr[j] === max && index != j) {
+    isSame = true;
+    break;
+  }
+}
+
+console.log(isSame ? "?" : String.fromCharCode(index + 65));
