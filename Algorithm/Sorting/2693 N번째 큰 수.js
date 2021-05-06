ef61227665d5a9393bce4b0414cd869f96a1f947
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-06 20:05:16 +0900

    # add : boj(2693)N번째 큰 수 문제풀이

---
+++ Algorithm/Sorting/2693 N번째 큰 수.js
@@ -0,0 +1,15 @@
+const input = require("fs")
+  .readFileSync("dev/stdin")
+  .toString()
+  .split("\n")
+  .map((x) =>
+    x
+      .split(" ")
+      .map((x) => +x)
+      .sort((a, b) => b - a)
+  );
+const T = input.shift();
+
+for (let i = 0; i < T; i++) {
+  console.log(input[i][2]);
+}
