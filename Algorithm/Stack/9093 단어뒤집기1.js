2c3bc9e9af10b7d7862b5bbee06d780e967a8f3d
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-06 17:36:25 +0900

    # add : boj(9093) 단어 뒤집기 문제 풀이

---
+++ Algorithm/Stack/9093 단어뒤집기1.js
@@ -0,0 +1,10 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+let n = +input.shift();
+let str = "";
+const rev = (word) => Array.from(word).reverse().join("");
+
+for (let i = 0; i < n; i++) {
+  let words = input[i].split(" ");
+  str += words.map(rev).join(" ") + "\n";
+}
+console.log(str);
