1e1f5b84c36304907afa3d288203c78a08ca0621
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-14 00:07:07 +0900

    # add : leetcode(53) 문제 풀이

--- Algorithm/LeetCode 75/53 Maximum SubArray.js
+++ Algorithm/LeetCode 75/53 Maximum SubArray.js
@@ -4,7 +4,7 @@ function solution() {
       nums[index] = value + nums[index - 1];
     }
   });
-  console.log(nums);
+
   return Math.max(...nums);
 }
 
@@ -12,20 +12,20 @@ let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 console.log(solution());
 
 // dp를 사용해서 문제를 풀었을 때
-// function solution() {
-//   let answer = 0;
-//   let dp = Array(nums.length).fill(0);
-//   dp[0] = nums[0];
+function solution() {
+  let answer = 0;
+  let dp = Array(nums.length).fill(0);
+  dp[0] = nums[0];
 
-//   for (let i = 1; i < nums.length; i++) {
-//     let prevDp = dp[i - 1] + nums[i];
-//     let prevNums = nums[i - 1] + nums[i];
-//     dp[i] = Math.max(nums[i], prevDp, prevNums);
-//   }
+  for (let i = 1; i < nums.length; i++) {
+    let prevDp = dp[i - 1] + nums[i];
+    let prevNums = nums[i - 1] + nums[i];
+    dp[i] = Math.max(nums[i], prevDp, prevNums);
+  }
 
-//   answer = Math.max(...dp);
-//   return answer;
-// }
+  answer = Math.max(...dp);
+  return answer;
+}
 
-// let nums = [-2, -1];
-// console.log(solution());
+let nums = [-2, -1];
+console.log(solution());
