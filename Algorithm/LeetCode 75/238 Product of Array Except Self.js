c4ca44b4b4385616704ef5d508c11ff2b5cfe894
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-14 16:57:14 +0900

    # add : leetcode(238) 문제 풀이

---
+++ Algorithm/LeetCode 75/238 Product of Array Except Self.js
@@ -0,0 +1,22 @@
+function solution(nums) {
+  const answer = [];
+
+  // 앞에서 뒤로 ( i 만나기 전 까지 )
+  for (let i = 0, temp = 1; i < nums.length; i++) {
+    answer[i] = temp;
+    temp *= nums[i];
+  }
+
+  // 뒤에서 앞으로 ( i 만나기 전 까지 )
+  for (let i = nums.length - 1, temp = 1; i >= 0; i--) {
+    answer[i] *= temp;
+    temp *= nums[i];
+  }
+  return answer;
+}
+
+let nums = [1, 2, 3, 4];
+let result = solution(nums);
+console.log(result);
+
+// How to make time Complexity to O(n)
