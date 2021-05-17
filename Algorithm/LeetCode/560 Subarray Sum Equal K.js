5456e50db9eb0a3bbbd77c60a44aa691d0a5a6d9
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-17 20:09:41 +0900

    # add : leetcode(two_Sum) 문제 풀이

---
+++ Algorithm/LeetCode/560 Subarray Sum Equal K.js
@@ -0,0 +1,6 @@
+function solution() {}
+
+let nums = [1, 1, 1];
+let k = 2;
+
+// O(n^2) => 하고 싶지 않아.
--- Algorithm/LeetCode/twoSum.js
+++ Algorithm/LeetCode/twoSum.js
@@ -1,3 +1,23 @@
+// 투포인터
+function twoPointer() {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let sum = nums[left] + nums[right];
+
+    if (sum < target) {
+      left++;
+    } else if (sum > target) {
+      right--;
+    } else {
+      console.log(left, right);
+      break;
+    }
+  }
+}
+
+// 이전 값을 저장.
 function solution() {
   const prev = [];
 
@@ -15,3 +35,4 @@ function solution() {
 let nums = [3, 2, 3];
 let target = 6;
 console.log(solution());
+console.log(twoPointer());
