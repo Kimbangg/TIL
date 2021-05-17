68dc9cfb44d2c15645d79720cf71794ddf01cd73
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-17 22:58:48 +0900

    # add : 프로그래머스 모의고사 문제 재풀이

--- Algorithm/BruteForce/프로그래머스 모의고사.js
+++ Algorithm/BruteForce/프로그래머스 모의고사.js
@@ -1,34 +1,60 @@
-function solution(answers) {
-    let max = 0;
-    let answer = [];
-    let patterns = [
-        [1,2,3,4,5],
-        [2,1,2,3,2,4,2,5],
-        [3,3,1,1,2,2,4,4,5,5]
-    ]
-    let point = [0,0,0]
-    
-    for(let i=0; i < answers.length; i++){
-        
-        if(answers[i] === patterns[0][i%5])
-            point[0]++;
-        
-        if(answers[i] === patterns[1][i%8])
-            point[1]++;
-        
-        if(answers[i] === patterns[2][i%10])
-            point[2]++;
-    }
+// function solution(answers) {
+//   let max = 0;
+//   let answer = [];
+//   let patterns = [
+//     [1, 2, 3, 4, 5],
+//     [2, 1, 2, 3, 2, 4, 2, 5],
+//     [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+//   ];
+//   let point = [0, 0, 0];
 
-    max = Math.max(point[0], point[1], point[2]);
-    
-    
-    for(let j=0; j<point.length; j++){
-        
-        if(max === point[j])
-            answer.push(j+1);
-        
+//   for (let i = 0; i < answers.length; i++) {
+//     if (answers[i] === patterns[0][i % 5]) point[0]++;
+
+//     if (answers[i] === patterns[1][i % 8]) point[1]++;
+
+//     if (answers[i] === patterns[2][i % 10]) point[2]++;
+//   }
+
+//   max = Math.max(point[0], point[1], point[2]);
+
+//   for (let j = 0; j < point.length; j++) {
+//     if (max === point[j]) answer.push(j + 1);
+//   }
+
+//   return answer;
+// }
+
+function solution(answers) {
+  let answer = [];
+  let max = 0;
+  let patterns = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
+
+  let count = [0, 0, 0];
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === patterns[0][i % 5]) count[0]++;
+
+    if (answers[i] === patterns[1][i % 8]) count[1]++;
+
+    if (answers[i] === patterns[2][i % 10]) count[2]++;
+  }
+
+  max = Math.max(...count);
+
+  count.forEach(function (each, index) {
+    if (each === max) {
+      answer.push(index + 1);
     }
-    
-    return answer;
+  });
+
+  return answer;
 }
+
+let answers = [1, 2, 3, 4, 5];
+
+console.log(solution(answers));
