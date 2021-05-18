9472425f65761a87601c4451fb60f0bdc143b5eb
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-18 10:58:48 +0900

    # add : 프로그래머스 폰켓몬 문제풀이

---
+++ Algorithm/Programmers/프로그래머스 폰켓몬.js
@@ -0,0 +1,24 @@
+// 해시를 이용한 문제 풀이
+function solution(nums) {
+  let map = {};
+  let maxCount = parseInt(nums.length / 2);
+
+  for (let i = 0; i < nums.length; i++) {
+    map[nums[i]] = (map[nums[i]] || 0) + 1;
+  }
+
+  let objectLength = Object.keys(map).length;
+
+  return objectLength <= maxCount ? objectLength : maxCount;
+}
+
+// set 함수를 이용한 문제풀이
+function solution(nums) {
+  let maxCount = parseInt(nums.length / 2);
+  const arr = [...new Set(nums)];
+
+  return arr.length <= maxCount ? arr.length : maxCount;
+}
+
+let nums = [3, 3, 3, 2, 2, 2];
+console.log(solution(nums));
