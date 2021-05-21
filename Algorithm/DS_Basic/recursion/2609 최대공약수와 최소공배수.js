625d12f31faee59ffc414fa3acbc7abdced0cc73
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-21 21:14:02 +0900

    # add : boj(2609)최대공약수와 최소공배수 문제풀이

--- Algorithm/DS_Basic/recursion/2609 최대공약수와 최소공배수.js
+++ Algorithm/DS_Basic/recursion/2609 최대공약수와 최소공배수.js
@@ -1,12 +1,7 @@
-function solution() {
+function solution(A, B) {
   let a = A;
   let b = B;
 
-  // 1112 % 695 = 417
-  // 695 % 417 = 278
-  // 417 % 278 = 139
-  // 287 % 139 = 0
-  // a = 139  b = 0 이 될 때 종료.
   while (b > 0) {
     let tmp = a % b;
     a = b;
@@ -19,4 +14,7 @@ function solution() {
 
 const input = require("fs").readFileSync("/dev/stdin", "utf8").trim();
 const [A, B] = input.split(" ").map((c) => +c);
-solution();
+const gcdNumber = solution(A, B);
+
+console.log(gcdNumber);
+console.log((A * B) / gcdNumber);
