991e2a8bef231d1c2a68915969fac926bdf89071
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-23 15:13:23 +0900

    # add : 프로그래머스(약수의 개수와 덧셈) 문제 풀이

---
+++ Algorithm/Programmers/프로그래머스 약수의 개수와 덧셈.js
@@ -0,0 +1,15 @@
+function solution(left, right) {
+  let answer = 0;
+  for (let i = left; i <= right; i++) {
+    if (Math.sqrt(i) % 1 === 0) {
+      answer -= i;
+    } else {
+      answer += i;
+    }
+  }
+  return answer;
+}
+
+let left = 13;
+let right = 17;
+console.log(solution(left, right));
