fc878177f14f8a211cc07ed1348e7b369238365e
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-23 21:13:01 +0900

    # add : 프로그래머스(네트워크) 문제 풀이

---
+++ Algorithm/Programmers/프로그래머스 네트워크.js
@@ -0,0 +1,35 @@
+function DFS(index, visited, computers) {
+  visited[index] = true;
+
+  for (let i = 0; i < computers.length; i++) {
+    if ((computers[index][i] === 1) & !visited[i]) {
+      DFS(i, visited);
+    }
+  }
+}
+
+function solution(n, computers) {
+  let answer = 0;
+  const visited = [];
+
+  for (const computer of computers) {
+    visited.push(false);
+  }
+
+  for (let i = 0; i < computers.length; i++) {
+    if (!visited[i]) {
+      DFS(i, visited, computers);
+      answer += 1;
+    }
+  }
+  return answer;
+}
+
+let n = 3;
+let computers = [
+  [1, 1, 0],
+  [1, 1, 0],
+  [0, 0, 1],
+];
+
+console.log(solution(n, computers));
