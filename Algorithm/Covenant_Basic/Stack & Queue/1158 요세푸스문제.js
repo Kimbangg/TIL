37ff628e4a9a76c12856739bd208559f286011e9
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-23 18:34:52 +0900

    # add : boj(1158) 요세푸스 문제 풀이

---
+++ Algorithm/Covenant_Basic/Stack & Queue/1158 요세푸스문제.js
@@ -0,0 +1,20 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const solution = (input) => {
+  const [N, K] = input[0].split(" ").map(Number);
+  const arr = Array.from({ length: N }, (v, i) => i + 1);
+
+  const ans = [];
+  let jump = 0;
+
+  while (arr.length > 0) {
+    jump += K - 1;
+    if (jump >= arr.length) jump %= arr.length;
+
+    ans.push(arr[jump]);
+    arr.splice(jump, 1);
+  }
+  return `<${ans.join(", ")}>`;
+};
+
+console.log(solution(input));
