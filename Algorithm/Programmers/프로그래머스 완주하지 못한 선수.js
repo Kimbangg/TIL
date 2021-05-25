1ca7b877e417af97656da8b119d1b3394b58cac9
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-25 13:10:40 +0900

    # add : 프로그래머스(완주하지 못한 선수) 재풀이

--- Algorithm/Programmers/프로그래머스 완주하지 못한 선수.js
+++ Algorithm/Programmers/프로그래머스 완주하지 못한 선수.js
@@ -1,3 +1,4 @@
+// 아래의 문제에 적용 되지 않는다.
 function solution(participant, completion) {
   let answer = "";
   participant.sort();
@@ -11,3 +12,14 @@ function solution(participant, completion) {
   }
   return answer;
 }
+
+// 동일인이 2명 일 때, 처리 해주지 못하는 문제 발생
+function solution(participant, completion) {
+  let answer = "";
+  answer += participant.filter((x) => !completion.includes(x));
+  return answer;
+}
+
+let participant = ["mislav", "stanko", "mislav", "ana"];
+let completion = ["stanko", "ana", "mislav"];
+console.log(solution(participant, completion));
