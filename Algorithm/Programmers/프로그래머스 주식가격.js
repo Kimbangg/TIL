42e1fe5ae3d3001d80a45312629450ff11eb6acf
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-25 14:37:39 +0900

    # add : 프로그래머스(주식 가격) 재 풀이

---
+++ Algorithm/Programmers/프로그래머스 주식가격.js
@@ -0,0 +1,19 @@
+function solution(prices) {
+  const answer = [];
+  let stack = [];
+
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[i] <= prices[j]) {
+        stack.push(1);
+      }
+    }
+
+    answer.push(stack.length);
+    stack = [];
+  }
+  return answer;
+}
+
+let prices = [1, 2, 3, 2, 3];
+console.log(solution(prices));
