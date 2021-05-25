40cb22d55e171ac4da920f3f0d08d6c721d213c9
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-05-25 22:26:59 +0900

    # docs : 파일 및 폴더 정리

--- Algorithm/Programmers/프로그래머스 소수찾기.js
+++ Algorithm/Programmers/프로그래머스 소수찾기.js
@@ -1,40 +1,52 @@
+function getPermutations() {}
+
 function solution(numbers) {
   let answer = 0;
-  numbers = numbers.split("");
-  let numCheck = new Set();
-  makeNumber("", numbers);
-
-  function isPrimeNumber(n) {
-    if (n < 2) {
-      return false;
-    }
-
-    for (let i = 2; i <= Math.sqrt(n); i++) {
-      if (n % i === 0) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  function makeNumber(cur, numbers) {
-    if (cur.length > 0) {
-      if (!numCheck.has(Number(cur))) {
-        numCheck.add(Number(cur));
-
-        if (isPrimeNumber(Number(cur))) {
-          answer++;
-        }
-      }
-    }
-
-    for (let i = 0; i < numbers.length; i++) {
-      let tmpArr = numbers.slice(0);
-      tmpArr.splice(i, 1);
-      makeNumber(cur + numbers[i], tmpArr);
-    }
-  }
+  let arr = numbers.split("");
 
   return answer;
 }
+
+let numbers = "17";
+console.log(solution(numbers));
+
+// function solution(numbers) {
+//   let answer = 0;
+//   numbers = numbers.split("");
+//   let numCheck = new Set();
+//   makeNumber("", numbers);
+
+//   function isPrimeNumber(n) {
+//     if (n < 2) {
+//       return false;
+//     }
+
+//     for (let i = 2; i <= Math.sqrt(n); i++) {
+//       if (n % i === 0) {
+//         return false;
+//       }
+//     }
+
+//     return true;
+//   }
+
+//   function makeNumber(cur, numbers) {
+//     if (cur.length > 0) {
+//       if (!numCheck.has(Number(cur))) {
+//         numCheck.add(Number(cur));
+
+//         if (isPrimeNumber(Number(cur))) {
+//           answer++;
+//         }
+//       }
+//     }
+
+//     for (let i = 0; i < numbers.length; i++) {
+//       let tmpArr = numbers.slice(0);
+//       tmpArr.splice(i, 1);
+//       makeNumber(cur + numbers[i], tmpArr);
+//     }
+//   }
+
+//   return answer;
+// }
