0c5a7ccbcc1d359afb5325b974f0a614a1ff0257
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-06-02 19:30:11 +0900

    # add : 이것이 취업을 위한 코딩테스트다 (DP) Problem Solve

--- Algorithm/NaDong/DP/효율적인 화폐 구성.js
+++ Algorithm/NaDong/DP/효율적인 화폐 구성.js
@@ -1,11 +1,39 @@
-function solution(coins) {
+// function solution(coins) {
+//   for (let i = 0; i < coins.length; i++) {
+//     for (let j = coins[i]; j <= target; j++) {
+//       if (dp[j - coins[i]] != 10001) {
+//         d[j] = Math.min(d[j].d[j - coins[i]] + 1);
+//       }
+//     }
+//   }
+// }
+
+// not using Dynamic Programming
+function solution() {
+  let count = 0;
+  coins = coins.sort((a, b) => b - a);
+
   for (let i = 0; i < coins.length; i++) {
-    for (let j = coins[i]; j <= target; j++) {
-      if (dp[j - coins[i]] != 10001) {
-        d[j] = Math.min(d[j].d[j - coins[i]] + 1);
-      }
+    if (target === 0) {
+      break;
+    }
+
+    if (coins[i] > target) {
+      continue;
+    }
+
+    if (target % coins[i] === 0) {
+      count += target / coins[i];
+      target %= coins[i];
+      console.log();
+      continue;
+    } else {
+      target -= coins[i];
+      count++;
     }
   }
+
+  return target !== 0 ? -1 : count;
 }
 
 let target = 15;
@@ -13,31 +41,3 @@ let coins = [2, 3];
 const dp = Array(target).fill(10001);
 dp[0] = 0;
 console.log(solution(coins));
-
-// not using Dynamic Programming
-// function solution() {
-//   let count = 0;
-//   coins = coins.sort((a, b) => b - a);
-
-//   for (let i = 0; i < coins.length; i++) {
-//     if (target === 0) {
-//       break;
-//     }
-
-//     if (coins[i] > target) {
-//       continue;
-//     }
-
-//     if (target % coins[i] === 0) {
-//       count += target / coins[i];
-//       target %= coins[i];
-//       console.log();
-//       continue;
-//     } else {
-//       target -= coins[i];
-//       count++;
-//     }
-//   }
-
-//   return target !== 0 ? -1 : count;
-// }
