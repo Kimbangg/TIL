c18f46db2413af046e625e04ae40f97580df9f22
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-06-13 17:49:01 +0900

    # add : boj(1182) 부분 수열의 합 Problem Solving

---
+++ Algorithm/BoostCamp/완전탐색(easy)/1182 부분수열의 합.js
@@ -0,0 +1,21 @@
+function dfs(idx, sum) {
+  if (idx >= n) {
+    return;
+  }
+
+  sum += nums[idx];
+
+  if (sum === target) {
+    count += 1;
+  }
+
+  dfs(idx + 1, sum - nums[idx]);
+  dfs(idx + 1, sum);
+}
+
+let count = 0;
+const [n, target] = [5, 0];
+const nums = [-7, -3, -2, 5, 8];
+const visited = Array(n + 1).fill(0);
+dfs(0, 0);
+console.log(count);
