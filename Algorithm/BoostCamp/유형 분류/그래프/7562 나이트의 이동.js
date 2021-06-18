232a5df9e4711115e53ee24473e432cfebd69d83
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-06-18 11:42:07 +0900

    # add : boj (7562) 나이트의 이동

--- Algorithm/BoostCamp/유형 분류/그래프/7562 나이트의 이동.js
+++ Algorithm/BoostCamp/유형 분류/그래프/7562 나이트의 이동.js
@@ -1,1 +1,54 @@
-function solve() {}
+function isValidRange(row, col) {
+  if (row < 0 || col < 0 || row >= n || col >= n) {
+    return false;
+  }
+
+  return true;
+}
+
+function BFS(x, y) {
+  const queue = [x, y];
+
+  while (queue.length) {
+    x = queue.shift();
+    y = queue.shift();
+
+    for (let i = 0; i < 8; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+
+      if (!isValidRange(nx, ny)) {
+        continue;
+      }
+
+      if (board[nx][ny] === 0) {
+        board[nx][ny] = board[x][y] + 1;
+        queue.push(nx, ny);
+      }
+    }
+  }
+}
+
+const [sx, sy] = [0, 0];
+const [ex, ey] = [7, 0];
+
+const dx = [1, 1, 2, 2, -1, -1, -2, -2];
+const dy = [-2, 2, -1, 1, -2, 2, -1, 1];
+
+const n = 8;
+
+const board = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+board[sx][sy] = 0;
+
+BFS(sx, sy);
+console.log(board[ex][ey]);
