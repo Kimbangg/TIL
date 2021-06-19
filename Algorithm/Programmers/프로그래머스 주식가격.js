3fe21c4cf4683ab38e4feb7832ea44242a2d9867
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-06-19 21:52:34 +0900

    # add : 프로그래머스 주식가격 문제풀이

--- Algorithm/Programmers/프로그래머스 주식가격.js
+++ Algorithm/Programmers/프로그래머스 주식가격.js
@@ -1,4 +1,36 @@
 function solution(prices) {
+  const stack = [];
+  const answer = Array(prices.length).fill(0);
+
+  for (let i = 0; i < prices.length; i++) {
+    // 주식 가격이 이전보다 낮음.
+    while (stack.length && prices[i] < prices[stack[stack.length - 1]]) {
+      // 이전에 저장 해놓은 idx 중 가장 위의 값을 뺸다.
+      // 그 이유는, 가장 위 == 가장 뒤에 등장한 녀석.
+      let temp = stack.pop();
+
+      // 현재 인덱스 값에서 뺀 인덱스를 값에 넣어 주면
+      // 몇 초 동안 유지 됬는지 알 수 있음.
+      answer[temp] = i - temp;
+    }
+    stack.push(i);
+  }
+
+  // 남아 있다는건, 자신보다 작아진 경우가 없다는 것.
+  // 최대 길이에서 자신의 인덱스를 제외
+  while (stack.length) {
+    let temp = stack.pop();
+    answer[temp] = prices.length - temp - 1;
+  }
+
+  console.log(answer);
+}
+
+let prices = [1, 2, 0, 0, 0];
+console.log(solution(prices));
+
+// 그래서 효율을 더 올릴 수 있는 방법은 없을까 ?
+function solution(prices) {
   const answer = [];
   let stack = [];
 
@@ -14,8 +46,3 @@ function solution(prices) {
   }
   return answer;
 }
-
-let prices = [1, 2, 3, 2, 3];
-console.log(solution(prices));
-
-// 그래서 효율을 더 올릴 수 있는 방법은 없을까 ?
