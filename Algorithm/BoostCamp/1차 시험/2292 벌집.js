d2e632d47fc7e58d7024c7804a88099115e1cd1a
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-06-24 11:48:21 +0900

    # add : bo (1193, 2292, 2775) 분수 찾기, 벌집, 부녀회장 Problem Solving

--- Algorithm/BoostCamp/1차 시험/2292 벌집.js
+++ Algorithm/BoostCamp/1차 시험/2292 벌집.js
@@ -1,12 +1,15 @@
 function solution() {
+  // 예외 조건으로 불 필요한 연산 막고
   if (target === 1) {
     return 1;
   }
 
-  let layer = 1; // room
-  let jump = 6; // plus
-  let layer_max_count = 1; // first
+  // 규칙을 어떻게 구현 할 것 인지?
+  let layer = 1;
+  let jump = 6;
+  let layer_max_count = 1;
 
+  // 예외로 발생 할 수 있는 문제를 체크.
   while (layer_max_count < target) {
     layer_max_count += jump;
     layer += 1;
