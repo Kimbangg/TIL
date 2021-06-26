3fbdbfd532f1e95fd29fa4d04b6c9ea9140f390e
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-06-26 19:24:49 +0900

    # add : 프로그래머스 야근 지수 Problem Solving

---
+++ Algorithm/Programmers/야근지수.js
@@ -0,0 +1,22 @@
+function solution(n, works) {
+  works.sort((a, b) => b - a);
+
+  let idx = 0;
+
+  while (n > 0) {
+    if (works[idx] < works[idx + 1]) {
+      idx += 1;
+      continue;
+    }
+
+    if (works[idx - 1] === works[idx]) {
+      idx = 0;
+      continue;
+    }
+
+    works[idx] -= 1;
+    n -= 1;
+  }
+
+  return works.reduce((acc, cur) => (cur > 0 ? acc + cur ** 2 : acc), 0);
+}
