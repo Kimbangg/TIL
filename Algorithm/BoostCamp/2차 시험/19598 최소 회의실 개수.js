f3e9183d0e70a34ff0fcf272eb5fb92c7ddb4507
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-06-28 10:46:13 +0900

    # add : 자바스크립트 비동기 처리 및 백준 문제 풀이

---
+++ Algorithm/BoostCamp/2차 시험/19598 최소 회의실 개수.js
@@ -0,0 +1,35 @@
+function solution() {
+  // const new_meeting = [];
+  // meeting.forEach((item) => {
+  //   new_meeting.push([item[0], 1]);
+  //   new_meeting.push([item[1], -1]);
+  // });
+  // new_meeting.sort((a, b) => {
+  //   if (a[0] === b[0]) return a[1] - b[1];
+  //   return a[0] - b[0];
+  // });
+  // let ans = 0;
+  // let cnt = 0;
+  // for (let i = 0; i < n * 2; i++) {
+  //   console.log(new_meeting[i]);
+  // }
+}
+
+let n = 3;
+let meeting = [
+  [0, 40],
+  [10, 20],
+  [5, 10],
+  [25, 50],
+  [40, 50],
+  [60, 70],
+];
+
+solution();
+
+// 몇 개의 회의실이 필요한지를 구하는거구나.
+
+// 10 20
+// 25 50
+// 40 50
+// 60 70
