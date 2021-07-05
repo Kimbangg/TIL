c1ec2638e9c881346c64194ebbd9bb9f8efc6fd1
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-07-05 12:16:10 +0900

    # add : 비동기 처리 및 서버/토큰 인증방식 정리

--- JavaScript/비동기.js
+++ JavaScript/비동기.js
@@ -2,28 +2,30 @@ const fs = require("fs");
 
 const aaa = () => {
   setTimeout(() => {
-    console.log("맨위-위");
+    console.log("맨위-위"); //d
   }, 0);
-  console.log("맨위-아래");
+  console.log("맨위-아래"); // c
 };
 
 setTimeout(() => {
   fs.readFile("any.txt", () => {
     setTimeout(() => {
-      console.log("중간-위");
+      console.log("중간-위"); // e
     }, 0);
     setImmediate(() => {
-      console.log("중간-아래");
+      console.log("중간-아래"); // f
     });
   });
 }, 100);
 
 setTimeout(() => {
-  console.log("마지막-위");
+  console.log("마지막-위"); // a
   aaa();
 }, 0);
 
 Promise.resolve().then(() => {
   aaa();
-  console.log("맨 아래");
+  console.log("맨 아래"); // b
 });
+
+// 맨위위 -> 중간위 -> 중간아래  -> 맨위위
