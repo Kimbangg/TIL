796be8dddfeb04b65c1f77cf5748f907f358f829
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-07-07 18:56:32 +0900

    # add : boj (10158, 16953) 개미, A -> B PS

--- Algorithm/boj/그리디/16953 A->B.js
+++ Algorithm/boj/그리디/16953 A->B.js
@@ -1,13 +1,23 @@
-let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-
 let count = 0;
 const [A, B] = input[0].split(" ").map((e) => +e);
+let result = -1;
+const queue = [A, 1];
 
-while (A !== B) {
-  count += 1;
+while (queue.length) {
+  let x = queue.shift();
+  let cnt = queue.shift();
+
+  if (x === B) {
+    break;
+  }
+
+  if (x * 2 <= B) {
+    queue.push(x * 2, cnt + 1);
+  }
+  if (x * 10 + 1 <= B) {
+    queue.push(x * 10 + 1, cnt + 1);
+  }
 }
-
-console.log(count);
 
 // 연산은 2가지만 가능하다
 // 2를 곱하거나, 1을 뒤에 붙이거나
