ba654d31d5d3c35b0a1664d2da7b6bc6a3fd49de
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-07-13 20:41:40 +0900

    # add : boj (18352) 특정 거리의 도시 찾기 PS

--- Algorithm/boj/최단거리/18352 특정 거리의 도시 찾기.py
+++ Algorithm/boj/최단거리/18352 특정 거리의 도시 찾기.py
@@ -1,38 +1,36 @@
 import sys
 from collections import deque
-
+ 
 input = sys.stdin.readline
-
+ 
 n, m, k, x = map(int, input().split())
-vistied = [False] * (n+1)
+visited = [False] * (n + 1)
+ 
+path = [[] for _ in range(n + 1)]
 
-path = [[] for _ in range(n + 1 )]
-
-for _ in range(m) :
-  a, b = map(int, input().split())
-  path[a].append(b)
+for _ in range(m):
+    a, b = map(int, input().split())
+    path[a].append(b)
 
 print(path)
-
+ 
 answer = list()
+queue = deque()
+queue.append((x, 0))
 
-q = deque()
-q.append((x,0))
-
-while q :
-  town, count = q.popleft()
-
-  if count == k :
-    answer.append(town)
-  elif count < k :
-    for con in path[town]:
-      if not vistied[con] :
-        vistied[con] = True
-        q.append((con, count+1))
-
+while queue:
+    town, count = queue.popleft()
+    if count == k:
+        answer.append(town)
+    elif count < k:
+        for con in path[town]:
+            if not visited[con]:
+                visited[con] = True
+                queue.append((con, count + 1))
+ 
 if len(answer) == 0:
-  print(-1)
-else :
-  answer.sort()
-  for ans in answer :
-    print(ans) 
+    print(-1)
+else:
+    answer.sort()
+    for ans in answer:
+        print(ans)
