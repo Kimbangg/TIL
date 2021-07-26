24fb5793b3f3392503e6bb35fd06f9c92b2d515b
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-07-26 11:26:16 +0900

    # add : boj (17298) 오큰수 PS

--- Algorithm/Study/17298 오큰수.py
+++ Algorithm/Study/17298 오큰수.py
@@ -1,11 +1,15 @@
 n = int(input())
 nums = list(map(int, input().split()))
+# 스택
 stack = []
-
+# 정답 배열을 -1로 초기화
 ans = [-1 for _ in range(n)]
 
 for i in range(len(nums)):
+    # 스택이 비어 있지 않고(= 이전에는 자신보다 큰 값을 만난 경우가 없고)
+    # i의 값이 스택 값보다 크다면 ( = 자신보다 큰 값을 만났을 때)
     while len(stack) != 0 and nums[stack[-1]] < nums[i]:
+        # i의 값으로 변경해준다.
         ans[stack.pop()] = nums[i]
     stack.append(i)
 
