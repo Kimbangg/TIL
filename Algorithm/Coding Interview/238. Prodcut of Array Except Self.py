89c5ca71d28f515063c69cc86a76008d73dc7faa
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-09-24 00:22:25 +0900

    # add : leetcode( 238 - Product of Array Except Self) PS

--- Algorithm/Coding Interview/238. Prodcut of Array Except Self.py
+++ Algorithm/Coding Interview/238. Prodcut of Array Except Self.py
@@ -1,14 +1,17 @@
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-         out = []
-          p = 1
-           # 왼쪽 곱셈
-           for i in range(0, len(nums)):
-                out.append(p)  # [ 1, 1, 2, 6 ]
-                p = p * nums[i]
-            p = 1
-            # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
-            for i in range(len(nums) - 1, 0 - 1, -1):
-                out[i] = out[i] * p
-                p = p * nums[i]
-            return out
+def solution(nums):
+    result = []
+    tmp = 1
+
+    for i in range(0, len(nums)):
+        result.append(tmp)
+        tmp *= nums[i]
+
+    tmp = 1
+
+    for i in range(len(nums) - 1, 0 - 1, -1):
+        result[i] *= tmp
+        tmp *= nums[i]
+
+
+nums = [1, 2, 3, 4]
+solution(nums)
